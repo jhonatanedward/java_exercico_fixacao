377c02b1053aa382a706ce3149d9864fcd7dab4c
Jhonatan Edward <jon.edward@outlook.com>
2020-07-15 12:21:13 -0300

    Entitidades criadas com base no diagrama UML

---
+++ src/entities/Order.java
@@ -0,0 +1,43 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+	private Date moment;
+	private OrderStatus status;
+	private Client client;
+	private List<OrderItem> items = new ArrayList<OrderItem>();
+	
+	public Order(Date moment, OrderStatus status) {
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	public void addItem(OrderItem item) {
+		this.items.add(item);
+	}
+	
+	public void removeItem(OrderItem item) {
+		this.items.remove(item);
+	}
+}
---
+++ src/entities/OrderItem.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class OrderItem {
+	private Integer quantity;
+	private Double price;
+	private Product product;
+	public Integer getQuantity() {
+		return quantity;
+	}
+	
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
